41170cf6be9c7ceb2f5b047a55f369b24298cb1a
Wouter Raateland <wouterraateland@gmail.com>
2021-01-16 00:06:04 +0100

    Simplify env usage

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const {
+  VERCEL_GITHUB_COMMIT_SHA,
+  VERCEL_GITLAB_COMMIT_SHA,
+  VERCEL_BITBUCKET_COMMIT_SHA,
+} = process.env;
+
+const COMMIT_SHA =
+  VERCEL_GITHUB_COMMIT_SHA ||
+  VERCEL_GITLAB_COMMIT_SHA ||
+  VERCEL_BITBUCKET_COMMIT_SHA;
+
+const basePath = "";
+
+module.exports = {
+  productionBrowserSourceMaps: true,
+  env: {
+    NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA,
+    NEXT_PUBLIC_PUBLIC_URL: process.env.PUBLIC_URL,
+    NEXT_PUBLIC_SUPABASE_API_URL: process.env.SUPABASE_API_URL,
+    NEXT_PUBLIC_SUPABASE_API_KEY: process.env.SUPABASE_API_KEY,
+  },
+  images: {
+    domains: [],
+  },
+  basePath,
+};
